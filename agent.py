c34ec605779c9c4a2d289f5608141986ac3619a2
ThugDuke <m1212333@gmail.com>
2018-07-04 14:53:45 +0800

    modify get_position_of_bases

--- agent.py
+++ agent.py
@@ -9,16 +9,23 @@ def get_position_of_bases():
     print("Please put the mouse at the lower right of game.")
     input("Press Enter to continue...")
     x1,y1 = pyautogui.position()
+    coordinates = []
 
-    for p in snp.locateAllOnScreen("base.png",threshold=0.95 ,region=(x,y,x1-x,y1-y)):
-        print(p[0],p[1])
-
+    for a in snp.locateAllOnScreen("base.png",threshold=0.9 ,region=(x,y,x1-x,y1-y)):
+        same_base = 0
+        for b in coordinates:
+            if abs(b[0]-a[0]) <=6 and abs(b[1]-a[1]) <=6:
+                same_base = 1
+        if same_base == 0:
+            coordinates.append((a[0], a[1]))
+        
+    return coordinates
+    #return list coord
     
-    #return list x
 
 def locate_and_hit(x):
     pass
-    #use list x
+    #use list coord
 
 def main():    
     get_position_of_bases()
